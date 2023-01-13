62f327e730380d4c40a4a5c430c1024dbe3a9ebf
Nuutti <70487373+nuutti0@users.noreply.github.com>
2023-01-13 20:34:22 +0200

    hamburger menu beta

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('navUL');
+const header = document.getElementById('navbar');
+
+hamburger.addEventListener('click', () => {
+    navUL.classList.toggle('show');
+    header.classList.toggle('show');
+});
