99937989c916ee7a57362d4b1bd0bc5a5112a19a
Nuutti <nuutti@nortunen.com>
2023-05-16 15:10:19 +0000

    projects accordion rough version

--- projects.js
+++ projects.js
@@ -1,15 +1,7 @@
+const accordion = document.getElementsByClassName('container');
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
 }
