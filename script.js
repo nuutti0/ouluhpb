0b814ecbdf255c108852be52d22ba7dbfce7deb6
Nuutti <nuutti@nortunen.com>
2024-05-07 15:54:07 +0000

    mobile fix

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const hamburger = document.getElementById('hamburger');
 const navUL = document.getElementById('navUL');
 const header = document.getElementById('navbar');
+const headerContainer = document.getElementById('hc');
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
     header.classList.toggle('show');
+    headerContainer.classList.toggle('show');
 });
 
 document.getElementById("year").innerHTML = new Date().getFullYear();
